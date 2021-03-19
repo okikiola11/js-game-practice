084ac24f52cd3cacc126364b45dd4b30fd6842e3
okikiola11 <okikiola.apelehin@gmail.com>
2021-03-19 18:33:51 +0000

    Initial set up Add the asset management files Install dev dependencies Add the separate file modules Add functions inside the module files

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import SceneMainMenu from '../src/js/sceneMainMenu';
+import game from '../src/js/game';
+
+SceneMainMenu();
+game;
---
+++ src/js/game.js
@@ -0,0 +1,22 @@
+// configuration properties for the Phaser game instance
+let config = {
+  type: Phaser.WEBGL,
+  width: 480,
+  height: 640,
+  backgroundColor: 'black',
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { x: 0, y:0 }
+    }
+  },
+  scene: [
+    SceneMainMenu,
+    SceneMain,
+    SceneGameOver
+  ],
+  pixelArt: true,
+  roundPixels: true
+}
+
+export const game = new Phaser.Game(config);
