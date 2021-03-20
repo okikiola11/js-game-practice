8cecc9ea70a254323cac4d2046ef139be9b75aac
okikiola11 <okikiola.apelehin@gmail.com>
2021-03-20 10:53:26 +0000

    Add the gunship to the scenemain class Add chasership and carriership class to the player class module

--- src/js/player.js
+++ src/js/player.js
@@ -48,15 +48,6 @@ class ChaserShip extends Entity {
   }
 }
 
-// new GunShip()
-// class GunShip extends Entity {
-//   constructor(scene, x, y) {
-//     super(scene, x, y, "sprEnemy0", "GunShip");
-//     this.play("sprEnemy0");
-//     this.body.velocity.y = Phaser.Math.Between(50, 100);
-//   }
-// }
-
 class CarrierShip extends Entity {
   constructor(scene, x, y) {
     super(scene, x, y, "sprEnemy2", "CarrierShip");
